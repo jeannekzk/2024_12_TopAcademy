d81a7a014d6ad75985c98da9d027a3b6a41929ed
jeannekzk <jeannrkazakova@gmail.com>
2024-12-20 21:27:40 +0300

    Factorial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int resul=1;
+    int key=0;
+
+    cin >> key;
+
+    for (int i=1; i<=key; i++){
+      resul *=i;
+    }
+    return 0;
+}
